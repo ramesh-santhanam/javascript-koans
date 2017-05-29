6624b306f291a09dbf6ddda338b40e1ba89e309f
Ramesh Santhanam <ramesh.santhanam@autodesk.com>
2017-05-28 23:06:54 -0400

    A start to WhatWeHaveLearnt

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,22 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
 
+    	for (i = 0; i < products.length; i+=1) {
+        	if (products[i].containsNuts === false) {
+
+		}	
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+	}
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
